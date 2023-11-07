16c7c227fb086b24c681b34ccd6a4c76252f6c11
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2023-11-07 01:39:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
 operador = input("Qual operação deseja? (+, -, *, /): ")
 num1 = float(input("Coloca o número 1: "))
 num2 = float(input("Coloca o número 2: "))
@@ -29,13 +19,3 @@ else:
     print("Operador inválido. Use apenas '+', '-', '*', '/'.")
 
 print("O resultado é: " + str(resultado))
-
-
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
